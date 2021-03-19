12f2ee12789f9965b8ee083b86a45542861fc466
sandrorvr <sandrorvr@yahoo.com.br>
2021-03-18 23:13:10 -0300

    create function isJson()

--- RandomTable.py
+++ RandomTable.py
@@ -7,17 +7,20 @@ class RandomTable(pd.DataFrame):
         self.df = df
 
 
-    def randommizeNotStr(self):
-        pass
+    def randommizeNotStr(self,el):
+
     def randommizeJson(self):
         pass
 
+    def isJson(self, el):
+        return True if (el[0]=='{' or el[1]=='{') and (el[-1]=='}' or el[-2]=='}') False
+
     def transform(self):
         for column in self.df.columns:
-            # se a coluna for do tipo != string
+            if self.df[column].dtype != np.object:
                 self.df[column].apply(self.randommizeNotStr)
-            #else:
-                # se coluna for um json:
+            else:
+                if self.isJson(self.df[column][0]) # se coluna for um json:
                     self.df[column].apply(self.randommizeJson)
-                #else:
+                else:
                     self.df[column].apply(lambda x: np.random.choice(list(x),len(x),replace=False))
