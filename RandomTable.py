fef7ebff04544e744c6af29dcf75e0b7aa714d8f
sandrorvr <sandrorvr@yahoo.com.br>
2021-03-19 08:35:22 -0300

    modify object

--- RandomTable.py
+++ RandomTable.py
@@ -3,12 +3,11 @@ import numpy as np
 
 class RandomTable(pd.DataFrame):
     def __init__(self,df):
-        super().__init__(data=None)
-        self.df = df
+        super().__init__(data=df)
 
 
     def randommizeNotStr(self,el,col):
-        return np.random.randint(self.df[col].min(), self.df[col].max())
+        return np.random.randint(self[col].min(), self[col].max())
 
     def randommizeJson(self,el):
         dic = Json.loads(el)
@@ -23,21 +22,12 @@ class RandomTable(pd.DataFrame):
         return True if (el[0]=='{' or el[1]=='{') and (el[-1]=='}' or el[-2]=='}') else False
 
     def transform(self):
-        for column in self.df.columns:
-            if self.df[column].dtype != np.object:
-                self.df[column] = self.df[column].apply(self.randommizeNotStr, col=column)
+        for column in self.columns:
+            if self[column].dtype != np.object:
+                self[column] = self[column].apply(self.randommizeNotStr, col=column)
             else:
-                if self.isJson(self.df[column][0]): # se coluna for um json:
-                    self.df[column] = self.df[column].apply(self.randommizeJson)
+                if self.isJson(self[column][0]): # se coluna for um json:
+                    self[column] = self[column].apply(self.randommizeJson)
                 else:
-                    self.df[column] = self.df[column].apply(lambda x: np.random.choice(list(x),len(x),replace=False))
-        return self.df
-
-
-df = pd.DataFrame(np.random.randint(0,1000,size=(10,10)))
-print('==========================')
-print(df)
-df.columns = list('abcdefghij')
-obj = RandomTable(df)
-data = obj.transform()
-print(data)
+                    self[column] = self[column].apply(lambda x: np.random.choice(list(x),len(x),replace=False))
+        return self
